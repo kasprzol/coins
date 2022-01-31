e4a16f4d450ec3964f178d5024d9205be10ba421
Maciej Kasprzyk <github@kasprzyk.email>
2022-01-31 20:55:21 +0100

    Ran black

--- CoinApp/apps.py
+++ CoinApp/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class CoinappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'CoinApp'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "CoinApp"
--- coins/asgi.py
+++ coins/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coins.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "coins.settings")
 
 application = get_asgi_application()
