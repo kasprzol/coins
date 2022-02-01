6199d3e8a1d3dc2b5e416b69c182d080f707b81e
Maciej Kasprzyk <github@kasprzyk.email>
2022-02-01 21:40:10 +0100

    Made GET and POST endpoints more conformant to the spec.

--- CoinApp/viewsets.py
+++ CoinApp/viewsets.py
@@ -1,5 +1,6 @@
 from http import HTTPStatus
 
+from django.http import Http404
 from rest_framework.response import Response
 from rest_framework.serializers import Serializer
 from rest_framework.viewsets import ModelViewSet
@@ -14,11 +15,36 @@ class ExchangeRateViewSet(ModelViewSet):
     serializer_class = ExchangeRateSerializer
 
     def list(self, request, *args, **kwargs):
-        return super().list(request, *args, **kwargs)
+        """Based on the specification the endpoint - return one rate instead of
+        list of all rates"""
+        queryset = self.filter_queryset(self.get_queryset())
+
+        obj = queryset.first()
+        if not obj:
+            raise Http404
+        serializer = self.get_serializer(obj)
+        return Response(serializer.data)
+
+    # disallow any other GET endpoints (e.g. /api/v1/quotes/3/)
+    def retrieve(self, request, *args, **kwargs):
+        raise Http404
 
     def create(self, request, *args, **kwargs):
         """Trigger fetching new quotes."""
+        if request.data:
+            return Response(
+                {"error": "Wasn't expecting any request body."},
+                status=HTTPStatus.BAD_REQUEST,
+            )
         fetch_exchange_rate.delay()
+        # https://developer.mozilla.org/en-US/docs/Web/HTTP/Status#client_error_responses
+        # 202 Accepted
+        #
+        # The request has been received but not yet acted upon. It is
+        # noncommittal, since there is no way in HTTP to later send an
+        # asynchronous response indicating the outcome of the request. It is
+        # intended for cases where another process or server handles the
+        # request, or for batch processing.
         return Response(status=HTTPStatus.ACCEPTED)
 
     def get_serializer_class(self):
