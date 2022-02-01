ddd3e1a5c26484bcc56ed0f1f27132a295e7b80f
Maciej Kasprzyk <github@kasprzyk.email>
2022-02-01 22:22:33 +0100

    Added djangorestframework-api-key package for API keys.

--- coins/settings.py
+++ coins/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "rest_framework_api_key",
     "CoinApp.apps.CoinappConfig",
 ]
 
@@ -136,6 +137,18 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+##########################
+# REST framework
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework_api_key.permissions.HasAPIKey",
+    ]
+}
+
+#########################
+# celery settings
 
 TASK_QUEUE_HOST = "queue"
 CELERY_BROKER_URL = f"redis://{TASK_QUEUE_HOST}/"
