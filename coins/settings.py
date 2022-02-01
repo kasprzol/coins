2fc346b0ec2836832ad5a25bc7c97241368625b8
Maciej Kasprzyk <github@kasprzyk.email>
2022-02-01 22:39:00 +0100

    Moved Django secret to environment variable.

--- coins/settings.py
+++ coins/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-&sbil%vi#b6sqc@nd)i1x-np)^m3^-zv%@9+!^(e!kxv6f$#=%"
+SECRET_KEY = os.environ["django_secret_key"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
