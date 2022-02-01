fe1f03fe1d9570585c6c6090daea5b8facc8450d
Maciej Kasprzyk <github@kasprzyk.email>
2022-02-01 13:57:47 +0100

    Optimized Dockerfiles for sharing layers.

--- docker/webapp_entrypoint.sh
+++ docker/webapp_entrypoint.sh
@@ -2,10 +2,10 @@
 
 set -euxo pipefail
 
-sleep 10 # give postgres time to start
+sleep 6 # give postgres time to start
 
-python3 manage.py migrate
+poetry run python3 manage.py migrate
 
 pwd
 
-gunicorn -b 0.0.0.0:8000 -w 4 coins.wsgi:application
+poetry run gunicorn -b 0.0.0.0:8000 -w 4 coins.wsgi:application
