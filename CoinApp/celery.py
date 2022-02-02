9798001f2a2a90c6660ed0e7f81cb57fdb874c61
Maciej Kasprzyk <github@kasprzyk.email>
2022-02-02 11:54:43 +0100

    Forgot to commit the celery-beat schedule.

--- CoinApp/celery.py
+++ CoinApp/celery.py
@@ -1,6 +1,7 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "coins.settings")
 
@@ -10,3 +11,11 @@ app.config_from_object("django.conf:settings", namespace="CELERY")
 
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
+
+
+app.conf.beat_schedule = {
+    "fetch-new-rates-every-hour": {
+        "task": "CoinApp.tasks.fetch_exchange_rate",
+        "schedule": crontab(hour="*", minute="20"),
+    },
+}
